0d3ad62644c448272706b784db81921db66d9678
sixdigit <mog@sixdigit.net>
2022-09-05 14:47:22 -0400

    fixed bug in populate_database.py
    remove file depends on demo name,
    call corresponding modules depends on demo name

--- docs/DB_sample_data/__init__.py
+++ docs/DB_sample_data/__init__.py
@@ -5,3 +5,10 @@ from __future__ import print_function
 
 __ENTITY_author__ = "SIX DIGIT INVESTMENT GROUP"
 __author__ = "GWONGZAN"
+
+from django.conf import settings
+
+if settings.DASHBOARD:
+    from . import dashboard_sample_data
+if settings.BLOG:
+    from . import blog_sample_data
--- docs/DB_sample_data/populate_database.py
+++ docs/DB_sample_data/populate_database.py
@@ -7,48 +7,62 @@ __ENTITY_author__ = "SIX DIGIT INVESTMENT GROUP"
 __author__ = "GWONGZAN"
 
 import os
-
 from django.conf import settings
+import docs
 
-from . import dashboard_sample_data
+modules = {}
 
-modules = {
-    'dashboard': dashboard_sample_data,
+if settings.DASHBOARD:
+    modules['dashboard'] = docs.DB_sample_data.dashboard_sample_data
+if settings.BLOG:
+    modules['blog'] = docs.DB_sample_data.blog_sample_data
+
+apps = {
+    'dashboard': ['accounts', 'api', 'sites'],
+    'blog': ['blog', ]
 }
 
 
-def clean_migrations_and_database():
+def clean_migrations_and_database(demo):
     # clean migrations and database
-    acct_migrations = os.path.join(settings.BASE_DIR, 'accounts', 'migrations')
-    sites_migrations = os.path.join(settings.BASE_DIR, 'sites', 'migrations')
+    migrations = []
+    for app in apps[demo]:
+        migrations.append(os.path.join(settings.BASE_DIR, app, 'migrations'))
+
     sqlite3_db = os.path.join(settings.BASE_DIR, 'db.sqlite3')
 
     if os.path.exists(sqlite3_db):
         os.remove(sqlite3_db)
+        print(f'{sqlite3_db} removed')
 
-    for f in os.listdir(acct_migrations):
-        if f.startswith('0'):
-            p = os.path.join(acct_migrations, f)
-            if os.path.exists(p):
-                os.remove(p)
-
-    for f in os.listdir(sites_migrations):
-        if f.startswith('0'):
-            p = os.path.join(sites_migrations, f)
-            if os.path.exists(p):
-                os.remove(p)
+    for m in migrations:
+        for f in os.listdir(m):
+            if f.startswith('0'):
+                p = os.path.join(m, f)
+                if os.path.exists(p):
+                    os.remove(p)
+                    print(f"{p} removed")
 
 
-def makemigration_and_migrate():
-    os.system('python manage.py makemigrations')
-    os.system('python manage.py migrate')
+def makemigration_and_migrate(demo):
+    print("start making migrations")
+    os.system(f'python manage.py makemigrations {" ".join(apps[demo])}')
+    print("start migration")
+    os.system(f'python manage.py migrate')
 
 
 def populate_sample_database(demo):
-    clean_migrations_and_database()
-    makemigration_and_migrate()
-    getattr(modules[demo], 'populate_database')()
+    if demo in modules:
+        clean_migrations_and_database(demo)
+        makemigration_and_migrate(demo)
+        print("start populating database")
+        getattr(modules[demo], 'populate_database')()
+        print("finished populating database. ")
+    else:
+        print(f"No demo installed: {demo}")
+    return
 
 
 if __name__ == "__main__":
-    populate_sample_database('dashboard')
+    # populate_sample_database('dashboard')
+    pass
